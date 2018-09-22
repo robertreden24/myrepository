69a75a53a1f91180ad0a7f6d58ab088b830d4b42
robertreden24 <43334133+robertreden24@users.noreply.github.com>
2018-09-22 23:11:04 +0700

    Update test.py

--- test.py
+++ test.py
@@ -18,9 +18,9 @@ for i in range(1,10,2):
 #diamond
 n = 4
 for i in range(n-1):
-    print((n-i) * ' ' + (2*i+1) *'*')
+    print((n-i) * " " + (2*i+1) *"*")
 for j in range(n-1, -1, -1):
-    print((n-j) * ' ' + (2*j+1) *'*')
+    print((n-j) * " " + (2*j+1) *"*")
 
 
 #reverse right-angle triangle
