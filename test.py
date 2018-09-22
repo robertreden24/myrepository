91e1d926b2a2ac2c0d48b3bf2cab15fd39262db2
robertreden24 <43334133+robertreden24@users.noreply.github.com>
2018-09-22 17:18:05 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,33 @@
+x= 1
+n= 5
+while ( x<= n):
+    print("*" * x)
+    x= x+1
+
+
+
+
+j=9
+for i in range(1,10,2):
+    print(" "*j+i*"*")
+    j=j-1
+
+
+
+
+n = 4
+for i in range(n-1):
+    print((n-i) * ' ' + (2*i+1) *'*')
+for j in range(n-1, -1, -1):
+    print((n-j) * ' ' + (2*j+1) *'*')
+
+
+num = 5
+for y in range(1,num+1):
+    for z in range(1,num+2):
+        print("*"* num)
+        num = num-1
+
+
+
+
